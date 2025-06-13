25697fe8ba40552657a4353027e152940385e4c7
Hari manikanta <punugulahari1@gmail.com>
2025-06-13 11:29:39 +0530

    Add files via upload

--- app/views.py
+++ app/views.py
@@ -8,8 +8,9 @@ from django.http import FileResponse
 from rest_framework.views import APIView
 from io import BytesIO
 # Create your views here.
-
-
+import speech_recognition as sr
+from pydub import AudioSegment
+import pickle
 
 
 class Pp(APIView):
@@ -50,3 +51,58 @@ class Ha(APIView):
 
         except Exception as e:
             return Response({"error": str(e)}, status=500)
+class Pa(APIView):
+    def post(self, request):
+        file1 = request.FILES.get("file1")
+        if not file1:
+            return Response({"error": "No file uploaded"}, status=400)
+
+        # Read uploaded file into memory
+        mp3_data = file1.read()
+
+        # Convert mp3 bytes to wav bytes using pydub and BytesIO
+        mp3_audio = AudioSegment.from_file(BytesIO(mp3_data), format="webm")  # or "mp3" if mp3 format
+        wav_io = BytesIO()
+        mp3_audio.export(wav_io, format="wav")
+        wav_io.seek(0)
+
+        # Initialize recognizer
+        reco = sr.Recognizer()
+
+        
+        with sr.AudioFile(wav_io) as source:
+            audio_data = reco.record(source)
+            try:
+                text = reco.recognize_google(audio_data)
+                print(text)
+            except sr.UnknownValueError:
+                text = "Could not understand audio"
+            except sr.RequestError as e:
+                text = f"Could not request results; {e}"
+                print(text)
+        return Response({"transcription": text})
+
+class Ps(APIView):
+    def post(self, request):
+        movie_name = request.data.get("movie_name")  # Get movie from request body
+
+        # Load data
+        movies = pickle.load(open(r'C:\Users\ADMIN\Desktop\m27back\new\app\moviesupdated.pkl', 'rb'))
+
+        similarity = pickle.load(open(r'C:\Users\ADMIN\Desktop\m27back\new\app\similarityupdated.pkl', 'rb'))
+
+        # Check if movie exists
+        if movie_name not in movies['title'].values:
+            return Response({"error": f"Movie '{movie_name}' not found."})
+
+        # Get index of the movie
+        idx = movies[movies['title'] == movie_name].index[0]
+
+        # Calculate similarity
+        distances = list(enumerate(similarity[idx]))
+        sorted_movies = sorted(distances, key=lambda x: x[1], reverse=True)[1:5]
+
+        # Build response
+        recommendations = [movies.iloc[i[0]].title for i in sorted_movies]
+        recommendations1 = [movies.iloc[i[0]].genre for i in sorted_movies]
+        return Response({"recommendations": recommendations,"recommendations1":recommendations1})
